66073bbb50464c4f07d10f57dc84aa558cc33b81
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2020-12-11 15:30:17 -0500

    Added some basic documentation.

--- asyncloud/client.py
+++ asyncloud/client.py
@@ -16,24 +16,39 @@ class SoundCloud:
         self.client_id = client_id
 
     async def user(self, user_id: int) -> SimpleNamespace:
-        """
-        Returns a user.
+        """Returns a user.
+
+        Args:
+            user_id (int): SoundCloud user ID
+
+        Returns:
+            SimpleNamespace: User
         """
         url = BASE_API_URL / "users" / str(user_id) % {"client_id": self.client_id}
         async with self.session.get(url) as res:
             return SimpleNamespace(**(await res.json()))
 
     async def track(self, track_id: int) -> SimpleNamespace:
-        """
-        Returns a track.
+        """Returns a track.
+
+        Args:
+            track_id (int): SoundCloud track ID
+
+        Returns:
+            SimpleNamespace: Track
         """
         url = BASE_API_URL / "tracks" / str(track_id) % {"client_id": self.client_id}
         async with self.session.get(url) as res:
             return SimpleNamespace(**(await res.json()))
 
     async def tracks(self, track_ids: List[int]) -> List[SimpleNamespace]:
-        """
-        Returns multiple tracks.
+        """Returns multiple tracks.
+
+        Args:
+            track_ids (List[int]): List of SoundCloud track IDs
+
+        Returns:
+            List[SimpleNamespace]: List of tracks
         """
         url = (
             BASE_API_URL
@@ -46,11 +61,19 @@ class SoundCloud:
         async with self.session.get(url) as res:
             return [SimpleNamespace(**track) for track in (await res.json())]
 
-    async def resolve(self, resource_url: str) -> SimpleNamespace:
+    async def resolve(self, sc_url: str) -> SimpleNamespace:
+        """Resolves soundcloud.com URLs to Resource URLs to use with the API.
+
+        Args:
+            sc_url (str): Soundcloud URL
+
+        Returns:
+            SimpleNamespace: SoundCloud resource
+        """
         url = (
             BASE_API_URL
             / "resolve"
-            % {"url": resource_url, "client_id": self.client_id}
+            % {"url": sc_url, "client_id": self.client_id}
         )
         async with self.session.get(url) as res:
             return SimpleNamespace(**(await res.json()))
