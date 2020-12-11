90be6efb8f72b1c859f13c4ef70db70118de1471
Kyle Anthony Williams <kyle.anthony.williams2@gmail.com>
2020-12-11 12:52:04 -0500

    Beginning construction on the client.

--- asyncloud/__init__.py
+++ asyncloud/__init__.py
@@ -1,1 +1,2 @@
+"""An asynchronous SoundCloud API wrapper that doesn't need an API key."""
 __version__ = '0.1.0'
---
+++ asyncloud/constants.py
@@ -0,0 +1,7 @@
+from yarl import URL
+from re import compile
+
+BASE_URL = URL("https://soundcloud.com")
+BASE_API_URL = URL("https://api-v2.soundcloud.com")
+SOUNDCLOUD_KEYGEN_URL_REGEX = compile(r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)")
+SOUNDCLOUD_API_KEY_REGEX = compile(r"(https:\/\/)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)")
